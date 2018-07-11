27571e5a0111cfcee6675f978c00dc13a86fdd41
nekocode <nekocode.cn@gmail.com>
2018-07-11 13:22:05 +0800

    Add comment to filter script

--- example/filter.groovy
+++ example/filter.groovy
@@ -1,4 +1,8 @@
-static String filter(String className, String methodName, String methodDesc) {
-    if (!className.startsWith('cn/nekocode')) return null // Skip this method
+static String filter(
+        String className, // Example: android/support/v4/app/Fragment
+        String methodName, // Example: getLayoutInflater
+        String methodDesc // Example: (Landroid/os/Bundle;)Landroid/view/LayoutInflater
+) {
+    if (!className.startsWith('cn/nekocode')) return null // Retrun null to skip this method
     return className.split('/').last() + '#' + methodName // Return tracing tag
 }
